e72697a6721c5cb06493f20874e8458b06957d3e
Santiago Traversa <santiago.traversa@gmail.com>
2018-10-18 18:04:19 -0300

    upgrade libs

--- test/factories/orders.rb
+++ test/factories/orders.rb
@@ -1,21 +1,21 @@
 FactoryBot.define do
   factory :order do
     address { { seamless_address: "383 Madison Avenue CONTACT YOUR MGR IF YOU DID NOT CONSUME THIS MEAL. DELIVER TO JPMC PREMISES ONLY Cross Streets: 46/47 City: New York, NY 10017 Apt/Flat/Suite/Floor #: 29 12126221127", company: "What A Company S.A.", address: "Street 123", geolocation: '40.748585,-74.00586', address_line_2: 'address line 2', zipcode: "10011" } }
-    delivery_instructions "Include napkins please!!!"
-    meals [{ item: "Viking Juice", price: "$10.00", quantity: 1, total: "$10.00" }, { item: "Great Hamburger", price: "$12.34", quantity: 4, total: "$24.68" }]
-    order_date(Date.today)
+    delivery_instructions { "Include napkins please!!!" }
+    meals { [{ item: "Viking Juice", price: "$10.00", quantity: 1, total: "$10.00" }, { item: "Great Hamburger", price: "$12.34", quantity: 4, total: "$24.68" }] }
+    order_date { Date.today }
     sequence(:order_number) { |n| n }
-    parsed true
-    product_total 11.50
-    sales_tax 1.15
-    delivery_fee 2.99
-    tip 2.30
-    total 17.94
+    parsed { true }
+    product_total { 11.50 }
+    sales_tax { 1.15 }
+    delivery_fee { 2.99 }
+    tip { 2.30 }
+    total { 17.94 }
     sequence(:user) { |n| { name: "John #{n} Doe", first_name: "John", last_name: "Doe", phone: "(564) 839-3450" } }
-    account "AccountName"
+    account { "AccountName" }
 
     factory :incomplete_order do
-      parsed false
+      parsed { false }
     end
   end
 end
